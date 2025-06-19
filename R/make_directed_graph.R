03e553587d34c44197fc6a7744809ab5abd53782
Min-gyoung Shin <18338399+wioxio@users.noreply.github.com>
2025-06-19 15:42:48 -0700

    Update make_directed_graph.R
    Error in make_graph():
    ! 'edges' must be numeric or character
    
    handled

--- R/make_directed_graph.R
+++ R/make_directed_graph.R
@@ -13,64 +13,48 @@
 #' make_directed_graph(network = your_network_object)
 #'
 #' @export
-make_directed_graph <- function(network,
-                                node_size = 5,
-                                label_genes = TRUE,
-                                label_size = 2) {
-  
-  # Load required packages
+make_directed_graph <- function (network, node_size = 5, label_genes = TRUE, label_size = 2) 
+{
   require(OmnipathR)
   require(igraph)
   require(GGally)
-  require(dplyr)
-  require(magrittr)
-  
-  # Import directional interactions from Omnipath
   interactions <- OmnipathR::import_omnipath_interactions()
   directions <- interactions[, c("source_genesymbol", "target_genesymbol")]
-  
-  # Get edge list from the provided network
   network_edges <- as.data.frame(get.edgelist(network), stringsAsFactors = FALSE)
   colnames(network_edges) <- c("from", "to")
-  
-  # Identify edges that match Omnipath interactions (in correct or reverse direction)
   edges_forward <- paste0(network_edges$from, "_", network_edges$to)
   edges_backward <- paste0(network_edges$to, "_", network_edges$from)
   omni_edges <- paste0(directions$source_genesymbol, "_", directions$target_genesymbol)
-  
-  # Identify edges to keep direction
-  directed_forward <- network_edges[edges_forward %in% omni_edges, ]
-  directed_backward <- network_edges[edges_backward %in% omni_edges, ]
-  
-  # Reverse backward edges
+  directed_forward <- network_edges[edges_forward %in% omni_edges, 
+  ]
+  directed_backward <- network_edges[edges_backward %in% omni_edges, 
+  ]
   if (nrow(directed_backward) > 0) {
     for (i in seq_len(nrow(directed_backward))) {
-      edge_id <- get.edge.ids(network, c(directed_backward$from[i], directed_backward$to[i]))
+      edge_id <- get.edge.ids(network, c(directed_backward$from[i], 
+                                         directed_backward$to[i]))
       network <- delete.edges(network, edge_id)
-      network <- add.edges(network, c(directed_backward$to[i], directed_backward$from[i]))
+      network <- add.edges(network, c(directed_backward$to[i], 
+                                      directed_backward$from[i]))
     }
   }
+  final_edges <- rbind(directed_forward, directed_backward[, 
+                                                           c("to", "from")])
   
-  # Finalize directed edges
-  final_edges <- rbind(directed_forward, directed_backward[, c("to", "from")])
-  
-  # Create final directed graph
-  directed_network <- graph_from_data_frame(final_edges, directed = TRUE)
-  
-  if(dim(get.edgelist(directed_network))[1]>0){
-    directed_network_simple <- igraph::simplify(directed_network, remove.multiple = TRUE, remove.loops = TRUE)
-    
-    # Plot the directed network
-    GGally::ggnet2(directed_network_simple,
-                   label = label_genes,
-                   size = node_size,
-                   label.size = label_size,
-                   color = "steelblue",
-                   arrow.size = 10,
-                   arrow.gap = 0.025)
-  }else{
-    print("There is no direction assgiend to edges")
+  if (nrow(final_edges) == 0) {
+    message("There are no directional edges in the network based on Omnipath.")
+    return(NULL)
   }
   
-
+  directed_network <- graph_from_data_frame(final_edges, directed = TRUE)
+  if (dim(get.edgelist(directed_network))[1] > 0) {
+    directed_network_simple <- igraph::simplify(directed_network, 
+                                                remove.multiple = TRUE, remove.loops = TRUE)
+    GGally::ggnet2(directed_network_simple, label = label_genes, 
+                   size = node_size, label.size = label_size, color = "steelblue", 
+                   arrow.size = 10, arrow.gap = 0.025)
+  }
+  else {
+    print("There is no direction assgiend to edges")
+  }
 }
